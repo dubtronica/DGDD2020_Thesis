f6023e25a9e275b283081c289b6fdd05177da085
Christian Jerald Chan <dubtronicaphil@gmail.com>
2019-09-03 17:53:10 +0800

    Add Ability.cs

---
+++ Assets/CharacterScripts/Ability.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability 
+{
+    string name, description;
+    double dmult;
+
+    public Ability()
+    {
+        name = "Punch";
+        description = "ONE PUUUUNCH!";
+        dmult = 2.2;
+    }
+
+    public Ability(string n, string d, double m)
+    {
+        name = n;
+        description = d;
+        dmult = m;
+    }
+
+    void perform()
+    {
+        Debug.Log("Yeeeet");
+    }
+}
