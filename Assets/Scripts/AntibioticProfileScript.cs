55693d244186ca9029abb91199f5073c2eff6d58
Jasmine Chan <chan.jasmine4@gmail.com>
2019-11-16 23:49:45 +0800

    Updated files from zipped file

---
+++ Assets/Scripts/AntibioticProfileScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AntibioticProfileScript : MonoBehaviour
+{
+    public ProfileElements prelems;
+
+    public string name, description;
+    public int hp, damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        upgrader.onClick.AddListener(upgrade);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        charactername.text = name;
+        HPValue.text = hp.ToString();
+        DMGValue.text = damage.ToString();
+        Description.text = description;
+    }
+
+    void upgrade()
+    {
+        hp *= 3;
+        damage *= 2;
+    }
+
+    [System.Serializable]
+    public class ProfileElements
+    {
+        public Text charactername, HPvalue, DMGvalue, Description;
+        public Button upgrader;
+    }
+
+    public Text charactername { get { return prelems.charactername; } }
+    public Text HPValue { get { return prelems.HPvalue; } }
+    public Text DMGValue { get { return prelems.DMGvalue; } }
+
+    public Text Description { get { return prelems.Description; } }
+
+    public Button upgrader { get { return prelems.upgrader; } }
+}
---
+++ Assets/Scripts/OTCProfileScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OTCProfileScript : MonoBehaviour
+{
+    public ProfileElements prelems;
+
+    public string name, description;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        charactername.text = name;
+        Description.text = description;
+    }
+
+    [System.Serializable]
+    public class ProfileElements
+    {
+        public Text charactername, Description;
+
+    }
+
+    public Text charactername { get { return prelems.charactername; } }
+    public Text Description { get { return prelems.Description; } }
+
+}
